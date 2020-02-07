c7acce9850e3854df9b9392d853a533a84ccc9be
sb <sb>
2020-02-07 12:35:20 -0500

    Initial Commit

---
+++ Homework2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework2
+//
+//  Created by Shawn Broadway on 2/7/20.
+//  Copyright © 2020 Shawn Broadway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
